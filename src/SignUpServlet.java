57daae3c08b7eb5902b1a88d07bbf46077dead47
Kshitij13579 <kshitijyerande@gmail.com>
2018-02-13 23:49:26 +0530

    signup servlet added with token generation

---
+++ src/SignUpServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.http.*;  
+import javax.servlet.*;  
+import java.io.*; 
+import java.util.*;
+import org.apache.commons.lang.RandomStringUtils;
+
+public class SignUpServlet extends HttpServlet{  
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException  
+	{  
+		res.setContentType("text/html");//setting the content type  
+		PrintWriter pw=res.getWriter();//get the stream to write the data  
+		String username=req.getParameter("user");
+		String pass=req.getParameter("pass");
+		String email=req.getParameter("email");
+		String contact=req.getParameter("contact");
+
+
+ 		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 10) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        String saltStr = salt.toString();
+
+			    
+		  
+		//writing html in the stream  
+
+		pw.println(saltStr);   
+		  
+		pw.close();//closing the stream  
+	}
+}  
