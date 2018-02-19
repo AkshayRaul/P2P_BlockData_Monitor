1f2bf5af99ded931fb075fa90a2196e5b78d8d7b
Kshitij13579 <kshitijyerande@gmail.com>
2018-02-20 00:05:00 +0530

    text file as database added token generation is to be added further

--- src/SignUpServlet.java
+++ src/SignUpServlet.java
@@ -6,10 +6,18 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse; 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
 
+import com.mongodb.client.*;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import com.mongodb.*;
 public class SignUpServlet extends HttpServlet{
-
+	//boolean auth;MongoClient mongo;  MongoDatabase db;
+	
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException
 	{
 		res.setContentType("text/html");//setting the content type
@@ -31,5 +39,69 @@ public class SignUpServlet extends HttpServlet{
 
 		pw.println(saltStr);
 		pw.close();//closing the stream
+		/*try{
+			MongoCredential credential; 
+      		credential = MongoCredential.createCredential("kshitijyerande", "blockchain","Blockdata6".toCharArray()); 
+			MongoClientOptions clientOptions = new MongoClientOptions.Builder().serverSelectionTimeout(50000).build();
+            mongo = new MongoClient(new ServerAddress("mongodb://Blockchain:Blockchain@ds239988.mlab.com:39988/blockchain"),Arrays.asList(credential),clientOptions);
+			System.out.println("connection successfull");
+			
+      		System.out.println("Connected to the database successfully");
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+
+		try{
+			db=mongo.getDatabase("blockchain");
+			 MongoCollection<Document> user = db.getCollection("User");
+			System.out.println("database accessed"); 
+			System.out.println("collection accessed"); 
+
+			Document document = new Document("id", saltStr) 
+      .append("username", username)
+      .append("password", pass) 
+      .append("email", email) 
+      .append("contact", contact); 
+      user.insertOne(document); 
+      System.out.println("Document inserted successfully");*/
+
+      //file insertion
+      FileWriter fw=null;
+      BufferedWriter bw=null;
+      File f=null;
+      String path="E:/apache/apache-tomcat-8.0.30/webapps/Blockchain/src/User.txt";
+      try{
+      		f=new File(path);
+      		if(f.exists()){
+      			System.out.println("file exists");
+      			fw = new FileWriter(path,true);
+      			 bw = new BufferedWriter(fw);
+      			bw.write(saltStr+","+username+","+pass+","+email+","+contact+"\n");
+
+
+      		}else{
+      			f.createNewFile();
+      			System.out.println("File created");
+      			fw = new FileWriter(path);
+      			 bw= new BufferedWriter(fw);
+      			bw.write(saltStr+","+username+","+pass+","+email+","+contact+"\n");
+
+      		}
+      }
+      catch(Exception e){
+			e.printStackTrace();
+	   }
+		finally{
+			bw.close();
+			fw.close();
+		
+		}
+
+
+			 
+		
+
+
 	}
 }
